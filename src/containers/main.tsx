b00594779faadafcc9848e99a66e47555ba6ee09
Alexandre Rieux <alexr.3165@gmail.com>
2017-02-13 23:37:03 +0000

    Add typescript map

--- src/containers/main.tsx
+++ src/containers/main.tsx
@@ -1,21 +1,24 @@
 import * as React from 'react';
-import { StyleSheet, css } from 'aphrodite/no-important';
-
-const styles = StyleSheet.create({
-  title: {
-    ':hover': {
-      color: 'blue'
-    }
-  }
-});
+import { Layer, Feature, Map } from 'react-mapbox-gl';
 
 export class Main extends React.Component<void, void> {
 
   public render() {
     return (
-      <div>
-        <h1 className={css(styles.title)}>Yeah it works</h1>
-      </div>
+      <Map
+        style="mapbox://styles/mapbox/streets-v8"
+        accessToken="pk.eyJ1IjoiZmFicmljOCIsImEiOiJjaWc5aTV1ZzUwMDJwdzJrb2w0dXRmc2d0In0.p6GGlfyV-WksaDV_KdN27A"
+        containerStyle={{
+          height: "100vh",
+          width: "100vw"
+        }}>
+          <Layer
+            type="symbol"
+            id="marker"
+            layout={{ "icon-image": "marker-15" }}>
+            <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+          </Layer>
+      </Map>
     );
   }
 }
